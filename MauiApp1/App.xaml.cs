5b5f889f5f7c44ff37dead71ae9447847c5d8c82
Robin Schroeder <robin.schroeder@mercmarine.com>
2024-10-14 11:23:52 -0500

    working example project in NR

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using NewRelic.MAUI.Plugin;
+
+namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+            CrossNewRelic.Current.HandleUncaughtException();
+            CrossNewRelic.Current.TrackShellNavigatedEvents();
+
+            // Set optional agent configuration
+            // Options are: crashReportingEnabled, loggingEnabled, logLevel, collectorAddress, crashCollectorAddress
+            // AgentStartConfiguration agentConfig = new AgentStartConfiguration(true, true, LogLevel.INFO, "mobile-collector.newrelic.com", "mobile-crash.newrelic.com");
+
+            if (DeviceInfo.Current.Platform == DevicePlatform.Android)
+            {
+                CrossNewRelic.Current.Start("AA8b50aab94b70a4b318cc17b6d0f43ef42453ac15-NRMA");
+                // Start with optional agent configuration 
+                // CrossNewRelic.Current.Start("<YOUR_ANDROID_TOKEN>", agentConfig);
+            }
+            else if (DeviceInfo.Current.Platform == DevicePlatform.iOS)
+            {
+                CrossNewRelic.Current.Start("<YOUR_IOS_TOKEN>");
+                // Start with optional agent configuration 
+                // CrossNewRelic.Current.Start("<YOUR_IOS_TOKEN>", agentConfig);
+            }
+        }
+    }
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+
+            NewRelic.MAUI.Plugin.CrossNewRelic.Current.LogDebug("OnCounterClicked");
+
+            var attr = new Dictionary<string, object>();
+            attr.Add("OnCounterClicked", count);
+            NewRelic.MAUI.Plugin.CrossNewRelic.Current.LogAttributes(attr);
+        }
+
+        private void OnCrashClicked(object sender, EventArgs e)
+        {
+            NewRelic.MAUI.Plugin.CrossNewRelic.Current.CrashNow();
+        }
+    }
+
+}
